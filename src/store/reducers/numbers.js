890a1fd0fd98f1b3682b988ad2e182019c2e2b1f
Diogo Oliveira <dluks82@gmail.com>
2021-11-08 19:51:00 -0300

    exercise ended

--- src/store/reducers/numbers.js
+++ src/store/reducers/numbers.js
@@ -1,3 +1,5 @@
+import { MIN_NUMBER_CHANGED, MAX_NUMBER_CHANGED } from '../actions/actionTypes'
+
 const initialState = {
     min: 0,
     max: 10
@@ -5,12 +7,12 @@ const initialState = {
 
 export default function numbersReducers(state = initialState, action) {
     switch (action.type) {
-        case 'MIN_NUMBER_CHANGED':
+        case MIN_NUMBER_CHANGED:
             return {
                 ...state,
                 min: action.payload
             }
-        case 'MAX_NUMBER_CHANGED':
+        case MAX_NUMBER_CHANGED:
             return {
                 ...state,
                 max: action.payload
